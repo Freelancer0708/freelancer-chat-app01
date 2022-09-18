565f5161d237b6c911c28367f2efbc1f8ad42138
Freelancer0708 <freelancernoaccont@gmail.com>
2022-09-18 23:15:59 +0900

    commit16

--- public/settings.js
+++ public/settings.js
@@ -1,5 +1,11 @@
 var main = document.getElementById("main"); 
+var msglist = document.getElementById("msglist"); 
 var height = window.innerHeight;
 console.log(main);
 console.log(height);
 main.style.height = height + "px";
+
+if(window.innerWidth <= 768) {
+    msglistheight = height - 240;
+    msglist.style.height = height + "px";
+}
