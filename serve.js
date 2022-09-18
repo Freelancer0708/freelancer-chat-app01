3e899525ebc9126fc015b1bac51be8b11213dc41
Freelancer0708 <freelancernoaccont@gmail.com>
2022-09-18 22:29:37 +0900

    commit12

--- serve.js
+++ serve.js
@@ -1,17 +1,25 @@
+const crypto = require('crypto');
 const app  = require("express")();
 const http = require("http").createServer(app);
 const io   = require("socket.io")(http);
+
+// HTMLやJSなどを配置するディレクトリ
+const DOCUMENT_ROOT = __dirname + "/public";
 
 /**
  * "/"にアクセスがあったらindex.htmlを返却
  */
 app.get("/", (req, res)=>{
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(DOCUMENT_ROOT + "/index.html");
 });
 
-app.get("/style.css", (req, res)=>{
-  res.sendFile(__dirname + "/style.css");
+app.get("/:file", (req, res)=>{
+  res.sendFile(DOCUMENT_ROOT + "/" + req.params.file);
 });
+
+// app.get("/style.css", (req, res)=>{
+//   res.sendFile(__dirname + "/style.css");
+// });
 
 /**
  * [イベント] ユーザーが接続
@@ -19,6 +27,20 @@ app.get("/style.css", (req, res)=>{
 io.on("connection", (socket)=>{
   console.log("ユーザーが接続しました");
 
+    //---------------------------------
+  // ログイン
+  //---------------------------------
+  (()=>{
+    // トークンを作成
+    const token = makeToken(socket.id);
+
+    // 本人にトークンを送付
+    io.to(socket.id).emit("token", {token:token});
+  })();
+
+  //---------------------------------
+  // 発言を全員に送信
+  //---------------------------------
   socket.on("post", (msg)=>{
     io.emit("member-post", msg);
   });
@@ -30,3 +52,14 @@ io.on("connection", (socket)=>{
  http.listen(process.env.PORT || 3000, ()=>{
   console.log("listening on *:3000");
 });
+
+/**
+ * トークンを作成する
+ *
+ * @param  {string} id - socket.id
+ * @return {string}
+ */
+ function makeToken(id){
+  const str = "aqwsedrftgyhujiko" + id;
+  return( crypto.createHash("sha1").update(str).digest('hex') );
+}
